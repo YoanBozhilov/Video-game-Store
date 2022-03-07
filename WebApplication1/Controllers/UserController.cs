826ed06ac9903db9c72d384a87983e593f31937f
YoanBozhilov <yoanbbozhilov@gmail.com>
2022-03-07 22:48:57 +0200

    almost implement CRUD operations

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserService userService;
+        
+        public UserController(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Data
+    {
+        public List<User> Users { get; set; }
+
+        public Data()
+        {
+            this.Users = new List<User>()
+            {
+                new User("Ivan", "Petrov", "ivanpetrov@gmail.com"),
+                new User("In", "Peov", "ivaetrov@gmail.com"),
+                new User("Ian", "Ptrov", "ivaov@gmail.com"),
+                new User("Ivn", "Prov", "itrov@gmail.com"),
+            };
+        }
+    }
+}
