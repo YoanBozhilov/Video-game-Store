999c149213503299efc8fcf22ed7b7a9cb2e2a89
YoanBozhilov <yoanbbozhilov@gmail.com>
2022-03-18 00:55:32 +0200

    improved feedback interface

---
+++ WebApplication2/Services/FeedbackService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.DTOs;
+using WebApplication2.Models;
+
+namespace WebApplication2.Services
+{
+    public class FeedbackService : IFeedbackService
+    {
+        private IData data;
+        private static int id = 4;
+
+        public FeedbackService(IData data)
+        {
+            this.data = data;
+        }
+        public void Create(FeedbackDTO feedbackDto)
+        {
+            Feedback feedback = toEntity(feedbackDto);
+            feedback.Id = ++id;
+            data.Feedbacks.Add(feedback);
+        }
+
+        public void Delete(int id)
+        {
+            Feedback feedback = this.GetEntityById(id);
+            data.Feedbacks.Remove(feedback);
+        }
+
+        public void Edit(int id, FeedbackDTO feedbackDto)
+        {
+            Feedback feedback = this.GetEntityById(id);
+            feedback.Email = feedbackDto.Email;
+            feedback.Name = feedbackDto.Name;
+            feedback.Message = feedbackDto.Message;
+
+        }
+
+        public List<FeedbackDTO> GetAll()
+        {
+            return data.Feedbacks
+                .Select(x => toDTO(x))
+                .ToList();
+        }
+
+        public FeedbackDTO GetById(int id)
+        {
+            return toDTO(GetEntityById(id));
+        }
+
+        private Feedback GetEntityById(int id)
+        {
+            return data.Feedbacks.FirstOrDefault(u => u.Id == id);
+        }
+
+        private Feedback toEntity(FeedbackDTO feedbackDTO)
+        {
+            return new Feedback(feedbackDTO.Id, feedbackDTO.Email, feedbackDTO.Name, feedbackDTO.Message);
+        }
+
+        private FeedbackDTO toDTO(Feedback feedback)
+        {
+            return new FeedbackDTO(feedback.Id, feedback.Email, feedback.Name, feedback.Message);
+        }
+    }
+}
+
---
+++ WebApplication2/Services/IFeedbackService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WebApplication2.DTOs;
+
+namespace WebApplication2.Services
+{
+    public interface IFeedbackService
+    {
+        void Create(FeedbackDTO feedbackDto);
+        void Delete(int id);
+        void Edit(int id, FeedbackDTO feedbackDto);
+        List<FeedbackDTO> GetAll();
+        FeedbackDTO GetById(int id);
+    }
+}
