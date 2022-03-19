6f0342abc36c9dd7450d69d84c5e2824ab62469c
YoanBozhilov <100837983+YoanBozhilov@users.noreply.github.com>
2022-03-19 15:18:34 +0200

    Delete WebApplication1 directory

--- WebApplication1/Models/IData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace WebApplication1.Models
-{
-    public interface IData
-    {
-        List<User> Users { get; set; }
-    }
-}
--- WebApplication1/Models/IDatafeedback.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace WebApplication1.Models
-{
-    public interface IDatafeedback
-    {
-        List<Feedback> Feedbacks { get; set; }
-    }
-}
