cd915e79658fd8618f199fa11b9b9c5f72bd9d5a
MartinApostolov <85119661+MartinApostolov@users.noreply.github.com>
2022-03-26 14:35:42 +0200

    vrzca

---
+++ WebApplication1/Services/GameServices.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class GameServices
+    {
+        
+            private AllDbContext dbContext;
+
+            public GameServices(AllDbContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+
+            public List<Game> GetAll()
+            {
+                return dbContext.Games.ToList();
+            }
+
+            public Game GetById(int id)
+            {
+                return dbContext.Games.FirstOrDefault(p => p.Id == id);
+            }
+
+            public void Create(Game game)
+            {
+                dbContext.Games.Add(game);
+                dbContext.SaveChanges();
+            }
+
+            public void Edit(Game game)
+            {
+                Game dbProduct = GetById(game.Id);
+
+                dbProduct.Name = game.Name;
+                dbProduct.Price = game.Price;
+                dbProduct.Description = game.Description;
+                dbProduct.Link = game.Link;
+                dbProduct.Pic = game.Pic;
+
+                dbContext.SaveChanges();
+            }
+
+            public void Delete(int id)
+            {
+                Game dbProduct = GetById(id);
+
+                dbContext.Games.Remove(dbProduct);
+                dbContext.SaveChanges();
+            }
+        
+    }
+}
